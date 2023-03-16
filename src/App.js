103f1a3a018f804561ae7cb4768316b7c7038461
s4llm4n <s4llm4n@gmail.com>
2023-03-16 14:00:06 +0700

    commit new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{ useState } from 'react';
+import Title from './components/Title'
+import Modal from './components/Modal';
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id:1},
+    {title: "bowser live stream", id:2},
+    {title: "race on moo moo farm", id:3}
+  ])
+
+  console.log(showEvents)
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id)
+  }
+
+  const subtitle = "All the latest events in Marioland"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Events in Your Area" subtitle={subtitle} />
+
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <React.Fragment key={event.id }>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </React.Fragment>
+      ))}
+
+      {/* <Modal>
+        <h2>10% Off Coupon Code!!</h2>
+        <p>Use the code NINJA10 at the checkout.</p>
+      </Modal> */}
+      <Modal>
+        <h2>Terms and Conditions</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, Error odit nam et
+          reprehenderit quibusdam temporibus officia dolorum quo sint nemo quis, laborum,
+          quasi nisi fugit praesentium debitis repudiandae! Sapiente, omnis.</p>
+          <a href="#">Find out more...</a>
+      </Modal>
+
     </div>
   );
 }
